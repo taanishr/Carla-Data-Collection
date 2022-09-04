f70f8e5434d7c966336d4602b4b97645abb2eaf9
SLevire <syliu1@asu.edu>
2022-09-03 22:45:14 -0700

    added class methods

--- GenerateLabelFile.py
+++ GenerateLabelFile.py
@@ -1,10 +1,103 @@
+from math import pi
+
 class Label:
     def __init__(self):
         self.class_name = ''
-        self.truncation = 0
-        self.occlusion = 0
-        self.alpha = 0
-        self.bounding_box = []
-        self.dimensions = []
-        self.location = []
-        self.rotation_y = 0
+        self.truncated = 0
+        self.occluded = 0
+        self.alpha = -10
+        self.bbox = None
+        self.dimensions = None
+        self.location = None
+        self.rotation_y = None
+        self.extent = None
+        
+    def set_type(self, obj_type: str):
+        self.type = obj_type
+    
+    def set_truncated(self, truncated: float):
+        self.truncated = truncated
+    
+    def set_occlusion(self, occlusion: int):
+        self._occluded = occlusion
+
+    def set_alpha(self, alpha: float):
+        assert -pi <= alpha <= pi, "Alpha must be in range [-pi..pi]"
+        self.alpha = alpha
+
+    def set_bbox(self, bbox: List[int]):
+        assert len(bbox) == 4, """ Bbox must be 2D bounding box of object in the image (0-based index):
+                     contains left, top, right, bottom pixel coordinates (two points)"""
+        self.bbox = bbox
+
+    def set_3d_object_dimensions(self, bbox_extent):
+        # Bbox extent consists of x,y and z.
+        # The bbox extent is by Carla set as
+        # x: length of vehicle (driving direction)
+        # y: to the right of the vehicle
+        # z: up (direction of car roof)
+        # However, Kitti expects height, width and length (z, y, x):
+        height, width, length = bbox_extent.z, bbox_extent.x, bbox_extent.y
+        # Since Carla gives us bbox extent, which is a half-box, multiply all by two
+        self.extent = (height, width, length)
+        self.dimensions = "{} {} {}".format(2*height, 2*width, 2*length)
+
+    def set_3d_object_location(self, obj_location):
+        """ TODO: Change this to 
+            Converts the 3D object location from CARLA coordinates and saves them as KITTI coordinates in the object
+            In Unreal, the coordinate system of the engine is defined as, which is the same as the lidar points
+            z
+            ▲   ▲ x
+            |  /
+            | /
+            |/____> y
+            This is a left-handed coordinate system, with x being forward, y to the right and z up 
+            See also https://github.com/carla-simulator/carla/issues/498
+            However, the camera coordinate system for KITTI is defined as
+                ▲ z
+               /
+              /
+             /____> x
+            |
+            |
+            |
+            ▼
+            y 
+            This is a right-handed coordinate system with z being forward, x to the right and y down
+            Therefore, we have to make the following changes from Carla to Kitti
+            Carla: X   Y   Z
+            KITTI:-X  -Y   Z
+        """
+        # Object location is four values (x, y, z, w). We only care about three of them (xyz)
+        x, y, z = [float(x) for x in obj_location][0:3]
+        assert None not in [
+            self.extent, self.type], "Extent and type must be set before location!"
+        if self.type == "Pedestrian":
+            # Since the midpoint/location of the pedestrian is in the middle of the agent, while for car it is at the bottom
+            # we need to subtract the bbox extent in the height direction when adding location of pedestrian.
+            y -= self.extent[0]
+        # Convert from Carla coordinate system to KITTI
+        # This works for AVOD (image)
+        #x *= -1
+        #y *= -1
+        #self.location = " ".join(map(str, [y, -z, x]))
+        #self.location = " ".join(map(str, [-x, -y, z]))
+        self.location = " ".join(map(str, [y, -z, x]))
+        # This works for SECOND (lidar)
+        #self.location = " ".join(map(str, [z, x, y]))
+        #self.location = " ".join(map(str, [z, x, -y]))
+
+    def set_rotation_y(self, rotation_y: float):
+        assert - \
+            pi <= rotation_y <= pi, "Rotation y must be in range [-pi..pi] - found {}".format(
+                rotation_y)
+        self.rotation_y = rotation_y
+
+    def __str__(self):
+        """ Returns the kitti formatted string of the datapoint if it is valid (all critical variables filled out), else it returns an error."""
+        if self.bbox is None:
+            bbox_format = " "
+        else:
+            bbox_format = " ".join([str(x) for x in self.bbox])
+
+        return "{} {} {} {} {} {} {} {}".format(self.type, self.truncated, self.occluded, self.alpha, bbox_format, self.dimensions, self.location, self.rotation_y)
